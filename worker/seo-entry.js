3d62c4bcc9d6077dcc2495be5eb08841e1cac10e
dawoodsa1 <dawoodsaeedkh1@gmail.com>
2026-09-15 18:46:20 +0300

    Serve sitemap as static asset and keep robots in Worker

--- worker/seo-entry.js
+++ worker/seo-entry.js
@@ -1,36 +1,7 @@
 import app from './index.js';
 
-const INDEXABLE_PATHS = ['', '/about.html', '/privacy.html', '/terms.html', '/copyright.html', '/contact.html'];
-
-function xmlEscape(value) {
-  return String(value)
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&apos;');
-}
-
 function originOf(request) {
   return new URL(request.url).origin.replace(/\/$/, '');
-}
-
-function sitemapResponse(request) {
-  const origin = originOf(request);
-  const urls = INDEXABLE_PATHS
-    .map(path => `  <url><loc>${xmlEscape(origin + (path || '/'))}</loc></url>`)
-    .join('\n');
-
-  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>\n`;
-
-  return new Response(body, {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/xml; charset=utf-8',
-      'Cache-Control': 'no-store',
-      'X-Content-Type-Options': 'nosniff'
-    }
-  });
 }
 
 function robotsResponse(request) {
@@ -50,10 +21,6 @@ function robotsResponse(request) {
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-
-    if (request.method === 'GET' && url.pathname === '/sitemap.xml') {
-      return sitemapResponse(request);
-    }
 
     if (request.method === 'GET' && url.pathname === '/robots.txt') {
       return robotsResponse(request);
